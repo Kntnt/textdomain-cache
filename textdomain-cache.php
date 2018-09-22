3b29b9ae19ce13975f35b58ddb3612cbfaa52cbc
Thomas Barregren <thomas@kntnt.se>
2018-09-22 20:48:26 +0200

    Initial commit.

---
+++ textdomain-cache.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @wordpress-plugin
+ * Plugin Name:       MO-file cache
+ * Plugin URI:        https://www.kntnt.com/
+ * Description:       Caches MO-files for faster load_textdomain(). Install an objct cache for best performance.
+ * Version:           1.0.0
+ * Author:            Per Soderlind
+ * Author URI:        https://soderlind.no
+ * License:           GPL-3.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ * Based on https://core.trac.wordpress.org/ticket/32052.
+ */
+
+add_filter( 'override_load_textdomain', function ( $retval, $domain, $mofile ) {
+
+	global $l10n;
+
+	if ( ! is_readable( $mofile ) ) return false;
+
+	$data = get_transient( md5( $mofile ) );
+	$mtime = filemtime( $mofile );
+
+	$mo = new MO();
+	if ( ! $data || ! isset( $data['mtime'] ) || $mtime > $data['mtime'] ) {
+		if ( ! $mo->import_from_file( $mofile ) ) return false;
+		$data = [
+			'mtime' => $mtime,
+			'entries' => $mo->entries,
+			'headers' => $mo->headers,
+		];
+		set_transient( md5( $mofile ), $data );
+	}
+	else {
+		$mo->entries = $data['entries'];
+		$mo->headers = $data['headers'];
+	}
+
+	if ( isset( $l10n[ $domain ] ) ) {
+		$mo->merge_with( $l10n[ $domain ] );
+	}
+
+	$l10n[ $domain ] = &$mo;
+
+	return true;
+
+}, 1, 3 );
